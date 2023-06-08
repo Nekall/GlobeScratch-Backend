e872f61235a096d85e3267819c6dbe185d9a0a90
Neka <benjamin.anezo@protonmail.com>
2023-06-08 11:45:15 +0200

    setup: add airtable

--- index.js
+++ index.js
@@ -3,15 +3,19 @@ import cors from "cors";
 import bodyParser from "body-parser";
 import dotenv from "dotenv";
 dotenv.config();
+import Airtable from 'airtable';
+const base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base(process.env.AIRTABLE_BASE_ID);
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-app.get("/", async (req, res) => {
+console.log(await base.table("users").select().all());
+
+app.get("/", async (_, res) => {
     res.send("GlobeScratch API is running...");
 });
 
 app.listen(PORT, () => {
     console.log(`server is ready on port: http://localhost:${PORT}`);
-  });
+});
