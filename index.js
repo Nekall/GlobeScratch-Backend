1200c683b690247ef1f5026b902830d4f0f76be3
Neka <benjamin.anezo@protonmail.com>
2023-06-08 16:02:53 +0200

    feat: start login route

--- index.js
+++ index.js
@@ -1,21 +1,79 @@
 import express from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
+import bcrypt from "bcrypt";
 import dotenv from "dotenv";
 dotenv.config();
-import Airtable from 'airtable';
-const base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base(process.env.AIRTABLE_BASE_ID);
+import Airtable from "airtable";
+const base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base(
+  process.env.AIRTABLE_BASE_ID
+);
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
-
-console.log(await base.table("users").select().all());
+app.use(cors());
+app.use(bodyParser.json());
 
 app.get("/", async (_, res) => {
-    res.send("GlobeScratch API is running...");
+  res.send("GlobeScratch API is running...");
+});
+
+app.post("/signup", async (req, res) => {
+  try {
+    const { username, email, password, lastname, firstname, country } =
+      req.body;
+    if (
+      !username ||
+      !email ||
+      !password ||
+      !lastname ||
+      !firstname ||
+      !country
+    ) {
+      return res
+        .status(400)
+        .json({ message: "Veuillez remplir tous les champs requis." });
+    }
+
+    const saltRounds = 10;
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+
+    await base.table("users").create([
+      {
+        fields: {
+          username,
+          email,
+          password: hashedPassword,
+          firstname,
+          lastname,
+          country,
+        },
+      },
+    ]);
+
+    res.status(200).json({ message: "Compte créé avec succès." });
+  } catch (error) {
+    console.error("Erreur lors de la création du compte:", error);
+    res
+      .status(500)
+      .json({
+        message: "Une erreur est survenue lors de la création du compte.",
+      });
+  }
+});
+
+app.post("/login", (req, res) => {
+  const { email, password } = req.body;
+
+  const user = {
+    email,
+  };
+
+  let token = jwt.sign({ user });
+  res.send(token);
 });
 
 app.listen(PORT, () => {
-    console.log(`server is ready on port: http://localhost:${PORT}`);
+  console.log(`server is ready on port: http://localhost:${PORT}`);
 });
