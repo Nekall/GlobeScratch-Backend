2831874474e0902ed5b699ce94afa6b1768d27df
Neka <benjamin.anezo@protonmail.com>
2023-06-09 10:26:24 +0200

    feat: login logic finished

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
 dotenv.config();
 import Airtable from "airtable";
@@ -21,10 +22,9 @@ app.get("/", async (_, res) => {
 
 app.post("/signup", async (req, res) => {
   try {
-    const { username, email, password, lastname, firstname, country } =
+    const { email, password, lastname, firstname, country } =
       req.body;
     if (
-      !username ||
       !email ||
       !password ||
       !lastname ||
@@ -33,7 +33,23 @@ app.post("/signup", async (req, res) => {
     ) {
       return res
         .status(400)
-        .json({ message: "Veuillez remplir tous les champs requis." });
+        .json({ 
+          success: false,
+          message: "Veuillez remplir tous les champs requis." });
+    }
+
+    const user = await base("users")
+      .select({
+        filterByFormula: `{email} = "${email}"`,
+      })
+      .all();
+    
+    if(user.length > 0) {
+      return res
+        .status(409)
+        .json({ 
+          success: false,
+          message: "Un utilisateur existe déjà avec cet email." });
     }
 
     const saltRounds = 10;
@@ -42,7 +58,6 @@ app.post("/signup", async (req, res) => {
     await base.table("users").create([
       {
         fields: {
-          username,
           email,
           password: hashedPassword,
           firstname,
@@ -52,12 +67,15 @@ app.post("/signup", async (req, res) => {
       },
     ]);
 
-    res.status(200).json({ message: "Compte créé avec succès." });
+    res.status(200).json({ 
+      success: true,
+      message: "Compte créé avec succès." });
   } catch (error) {
     console.error("Erreur lors de la création du compte:", error);
     res
       .status(500)
       .json({
+        success: false,
         message: "Une erreur est survenue lors de la création du compte.",
       });
   }
@@ -66,12 +84,70 @@ app.post("/signup", async (req, res) => {
 app.post("/login", (req, res) => {
   const { email, password } = req.body;
 
-  const user = {
-    email,
-  };
+  if (!email || !password)
+    return res
+      .status(400)
+      .json({ 
+        success: false,
+        message: "Veuillez remplir tous les champs requis." });
+  
+  let user = base("users")
+    .select({
+      filterByFormula: `{email} = "${email}"`,
+    })
+    .all((err, records) => {
+      if(err) {
+        console.error("Erreur lors de la récupération de l'utilisateur:", err);
+        return res
+          .status(500)
+          .json({
+            success: false,
+            message: "Une erreur est survenue lors de la récupération de l'utilisateur.",
+          });
+      }
+      if(records.length === 0) {
+        return res
+          .status(404)
+          .json({
+            success: false,
+            message: "Aucun utilisateur trouvé avec cet email.",
+          });
+      }
+      const user = records[0].fields;
+      bcrypt.compare(password, user.password, (err, result) => {
+        if(err) {
+          console.error("Erreur lors de la comparaison des mots de passe:", err);
+          return res
+            .status(500)
+            .json({
+              success: false,
+              message: "Une erreur est survenue lors de la comparaison des mots de passe.",
+            });
+        }
+        if(!result) {
+          return res
+            .status(401)
+            .json({
+              success: false,
+              message: "Mot de passe incorrect.",
+            });
+        }
 
-  let token = jwt.sign({ user });
-  res.send(token);
+        delete user.password;
+        const token = jwt.sign({ user }, process.env.JWT_SECRET, {
+          expiresIn: "30d",
+        });
+        res
+          .status(200)
+          .json({
+            success: true,
+            message: "Connexion réussie.",
+            token,
+            user,
+            });
+      
+      });
+    });
 });
 
 app.listen(PORT, () => {
