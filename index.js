3ee6965d7a0983301016895f10ba725f6002fba2
Neka <benjamin.anezo@protonmail.com>
2023-06-15 17:01:37 +0200

    feat: add login form & all refacto

--- index.js
+++ index.js
@@ -18,26 +18,127 @@ app.use(cors());
 app.use(bodyParser.json());
 
 app.get("/", async (_, res) => {
-  res.send("GlobeScratch API is running...");
+  const form = `
+    <style>
+      .container {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 100vh;
+      }
+
+      .form-container {
+        max-width: 400px;
+        padding: 20px;
+        border: 1px solid #ccc;
+        border-radius: 4px;
+        background-color: #f2f2f2;
+      }
+
+      .form-container label {
+        display: block;
+        margin-bottom: 10px;
+        font-weight: bold;
+      }
+
+      .form-container input[type="text"],
+      .form-container input[type="password"] {
+        width: 100%;
+        padding: 10px;
+        margin-bottom: 20px;
+        border: 1px solid #ccc;
+        border-radius: 4px;
+        box-sizing: border-box;
+      }
+
+      .form-container input[type="submit"] {
+        background-color: #4caf50;
+        color: white;
+        padding: 10px 20px;
+        border: none;
+        border-radius: 4px;
+        cursor: pointer;
+      }
+
+      .error-message {
+        max-width: 400px;
+        padding: 20px;
+        border: 1px solid #ff3333;
+        border-radius: 4px;
+        background-color: #ffe6e6;
+        color: #ff3333;
+        font-weight: bold;
+      }
+    </style>
+    
+    <div class="container">
+      <div class="form-container">
+        <form action="/error" method="GET">
+          <label for="username">Nom d'utilisateur:</label>
+          <input type="text" id="username" name="username" required>
+
+          <label for="password">Mot de passe:</label>
+          <input type="password" id="password" name="password" required>
+
+          <input type="submit" value="Se connecter">
+        </form>
+      </div>
+    </div>
+  `;
+
+  res.send(form);
+});
+
+app.get("/error", async (_, res) => {
+  const errorMessage = `
+    <style>
+      .container {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 100vh;
+      }
+
+      .error-message {
+        max-width: 400px;
+        padding: 20px;
+        border: 1px solid #ff3333;
+        border-radius: 4px;
+        background-color: #ffe6e6;
+        color: #ff3333;
+        font-weight: bold;
+      }
+    </style>
+    
+    <div class="container">
+      <div class="error-message">
+        Erreur : nom d'utilisateur ou mot de passe incorrect !
+      </div>
+    </div>
+  `;
+
+  res.send(errorMessage);
 });
 
 app.post("/signup", async (req, res) => {
   try {
     const { email, password, lastname, firstname, country } = req.body;
-    if (!email || !password || !lastname || !firstname || !country) {
+    const requiredFields = [email, password, lastname, firstname, country];
+
+    if (requiredFields.some((field) => !field)) {
       return res.status(400).json({
         success: false,
         message: "Veuillez remplir tous les champs requis.",
       });
     }
 
-    const user = await base("users")
+    const existingUser = await base("users")
       .select({
         filterByFormula: `{email} = "${email}"`,
       })
       .all();
 
-    if (user.length > 0) {
+    if (existingUser.length > 0) {
       return res.status(409).json({
         success: false,
         message: "Un utilisateur existe déjà avec cet email.",
@@ -76,11 +177,12 @@ app.post("/signup", async (req, res) => {
 app.post("/login", (req, res) => {
   const { email, password } = req.body;
 
-  if (!email || !password)
+  if (!email || !password) {
     return res.status(400).json({
       success: false,
       message: "Veuillez remplir tous les champs requis.",
     });
+  }
 
   base("users")
     .select({
@@ -95,13 +197,16 @@ app.post("/login", (req, res) => {
             "Une erreur est survenue lors de la récupération de l'utilisateur.",
         });
       }
-      if (records.length === 0) {
+
+      const user = records[0]?.fields;
+
+      if (!user) {
         return res.status(404).json({
           success: false,
           message: "Aucun utilisateur trouvé avec cet email.",
         });
       }
-      const user = records[0].fields;
+
       bcrypt.compare(password, user.password, (err, result) => {
         if (err) {
           console.error(
@@ -114,6 +219,7 @@ app.post("/login", (req, res) => {
               "Une erreur est survenue lors de la comparaison des mots de passe.",
           });
         }
+
         if (!result) {
           return res.status(401).json({
             success: false,
@@ -122,9 +228,11 @@ app.post("/login", (req, res) => {
         }
 
         delete user.password;
+
         const token = jwt.sign({ user }, process.env.JWT_SECRET, {
           expiresIn: "30d",
         });
+
         res.status(200).json({
           success: true,
           message: "Connexion réussie.",
@@ -136,8 +244,6 @@ app.post("/login", (req, res) => {
 });
 
 app.put("/user", async (req, res) => {
-  console.log(req.body);
-
   const jwtToken = req.headers.authorization;
 
   if (!jwtToken) {
@@ -158,11 +264,11 @@ app.put("/user", async (req, res) => {
   }
 
   try {
+    const userEmail = jwt.decode(jwtToken.split(" ")[1]).user.email;
+
     let user = await base("users")
       .select({
-        filterByFormula: `{email} = "${
-          jwt.decode(jwtToken.split(" ")[1]).user.email
-        }"`,
+        filterByFormula: `{email} = "${userEmail}"`,
       })
       .all();
 
@@ -173,34 +279,28 @@ app.put("/user", async (req, res) => {
       });
     }
 
+    const updateUserFields = {
+      email: req.body.email || user[0].fields.email,
+      password: req.body.password
+        ? await bcrypt.hash(req.body.password, saltRounds)
+        : user[0].fields.password,
+      firstname: req.body.firstname || user[0].fields.firstname,
+      lastname: req.body.lastname || user[0].fields.lastname,
+      country: req.body.country || user[0].fields.country,
+      franceDpt: req.body.franceDpt || user[0].fields.franceDpt,
+      countries: req.body.countries || user[0].fields.countries,
+    };
+
     await base("users").update([
       {
         id: user[0].id,
-        fields: {
-          email: req.body.email ? req.body.email : user[0].fields.email,
-          password: req.body.password
-            ? await bcrypt.hash(req.body.password, saltRounds)
-            : user[0].fields.password,
-          firstname: req.body.firstname
-            ? req.body.firstname
-            : user[0].fields.firstname,
-          lastname: req.body.lastname
-            ? req.body.lastname
-            : user[0].fields.lastname,
-          country: req.body.country ? req.body.country : user[0].fields.country,
-          franceDpt: req.body.franceDpt
-            ? req.body.franceDpt
-            : user[0].fields.franceDpt,
-          countries: req.body.countries ? req.body.countries : user[0].fields.countries,
-        },
+        fields: updateUserFields,
       },
     ]);
 
     user = await base("users")
       .select({
-        filterByFormula: `{email} = "${
-          jwt.decode(jwtToken.split(" ")[1]).user.email
-        }"`,
+        filterByFormula: `{email} = "${userEmail}"`,
       })
       .all();
 
@@ -241,11 +341,11 @@ app.delete("/user", async (req, res) => {
   }
 
   try {
+    const userEmail = jwt.decode(jwtToken.split(" ")[1]).user.email;
+
     let user = await base("users")
       .select({
-        filterByFormula: `{email} = "${
-          jwt.decode(jwtToken.split(" ")[1]).user.email
-        }"`,
+        filterByFormula: `{email} = "${userEmail}"`,
       })
       .all();
 
@@ -272,5 +372,5 @@ app.delete("/user", async (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`server is ready on port: http://localhost:${PORT}`);
+  console.info(`server is ready on port: http://localhost:${PORT}`);
 });
